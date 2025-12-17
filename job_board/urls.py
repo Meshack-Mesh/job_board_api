9b6db8ad3f7059c8013a6f7c912daa0fc00365bc
Meshack-Mesh <michael.meshack21@students.dkut.ac.ke>
2025-12-17 17:44:00 +0300

    Implemeted templates and permissions for admin logins

--- job_board/urls.py
+++ job_board/urls.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -11,6 +14,18 @@ urlpatterns = [
     path('auth/', include('users.urls')),
     path('auth/login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('auth/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('', include('frontend.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
 
+
+    path('', include('jobs.urls')),
     path('', include('companies.urls')),
+    path('', include('applications.urls')),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL,
+        document_root=settings.MEDIA_ROOT
+    )
